3d47024855aaf45fbae8f1da77017de11204c6aa
user <ujjalroy7862@gmail.com>
2022-12-31 17:06:34 +0600

    done the app

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -16,9 +16,28 @@ const Home = () => {
         sort === "lowToHigh" ? a.price - b.price : b.price - a.price
       );
     }
+
+    if (!byStock) {
+      sortedProducts = sortedProducts.filter((product) => product.inStock);
+    }
+
+    if (byFastDelivery) {
+      sortedProducts = sortedProducts.filter((product) => product.fastDelivery);
+    }
+    if (byRating) {
+      sortedProducts = sortedProducts.filter(
+        (product) => product.ratings >= byRating
+      );
+    }
+
+    if (searchQuery) {
+      sortedProducts = sortedProducts.filter((product) =>
+        product.name.toLowerCase().includes(searchQuery)
+      );
+    }
     return sortedProducts;
   };
-  
+
   return (
     <div className="home">
       <Filter />
